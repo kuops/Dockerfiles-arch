1cd7e454b9af5713618dabd6903c2e9ffde3ae38
kuops <opshsy@gmail.com>
2018-06-16 05:25:22 -0400

    add gcrsync

--- gcrsync/get_image_tag.sh
+++ gcrsync/get_image_tag.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 1 ];then
-    echo "Usage: $0 k8s.gcr.io/Imagename"
+    echo "Usage: $0 Imagename"
     exit 177
 fi
+
 
 GCR_IMAGE_NAME=$1
 GCR_IMAGE_TAG=$(curl -sSL https://k8s.gcr.io/v2/${GCR_IMAGE_NAME}/tags/list|jq .tags[])
 GCR_TAG_ARRAY=($(echo "$GCR_IMAGE_TAG"|xargs))
 CURRENT_TAG_ARRAY=($(git tag|xargs))
 
-# clean git tag
-for i in ${GCR_TAG_ARRAY[@]};do
-        if [[ " ${CURRENT_TAG_ARRAY[@]} " =~ " ${i} " ]]; then
-                git config --global user.name "kuops"
-                git config --global user.email opshsy@gmail.com
-                git tag --delete $i
-                git push origin :$i
-        fi
-done
 # add git tag
 for i in ${GCR_TAG_ARRAY[@]};do
 	if [[ ! " ${CURRENT_TAG_ARRAY[@]} " =~ " ${i} " ]]; then
